3dae05374705d764c0469c94cae5f819e9c26e9f
cluu <cluu0655@gmail.com>
2025-07-08 18:26:35 -0700

    started manually setting up files

--- test.py
+++ test.py
@@ -7,22 +7,29 @@ fclip = FashionCLIP("fashion-clip")
 
 # GAP product
 imageA = Image.open("images/baggy_shorts.avif")
-# text = "The roomiest pair in your rotation. Extra loose, seriously cool & brings all the vibes. Fit: Extra roomy through the hip & thigh with a baggy, slightly tapered leg. Fabric: 100% Regenerative Cotton. Stretch: No Stretch. Authentic rigid denim that gets better with every wear. Made to wear all day & break in over time.​ Look: Five-pocket denim shorts in a black wash. Details: Zip fly & five-pocket styling."
+text = "The roomiest pair in your rotation. Extra loose, seriously cool & brings all the vibes. Fit: Extra roomy through the hip & thigh with a baggy, slightly tapered leg. Fabric: 100% Regenerative Cotton. Stretch: No Stretch. Authentic rigid denim that gets better with every wear. Made to wear all day & break in over time.​ Look: Five-pocket denim shorts in a black wash. Details: Zip fly & five-pocket styling."
 
 imageB = Image.open("images/baggy_black.webp")
 
+# print("Image A mode:", imageA.mode, "size:", imageA.size)
+# print("Image B mode:", imageB.mode, "size:", imageB.size)
+# imageA.show()
+
 # Get embeddings
 imageA_emb = fclip.encode_images([imageA], batch_size=1)  # shape: (1, 512)
-# text_emb = fclip.encode_text([text], batch_size=1)             # shape: (1, 512)
+text_emb = fclip.encode_text([text], batch_size=1)             # shape: (1, 512)
 
 # Combine (simple average)
-# combined_emb = (imageA_emb + text_emb) / 2
+combined_emb = (imageA_emb + text_emb) / 2
 
 imageB_emb = fclip.encode_images([imageB], batch_size=1)  # shape: (1, 512)
 
 
-# similarity = cosine_similarity(combined_emb, imageB_emb)
-similarity = cosine_similarity(imageA_emb, imageB_emb)
+
+
+
+similarity = cosine_similarity(combined_emb, imageB_emb)
+#similarity = cosine_similarity(imageA_emb, imageB_emb)
 
 print(f"Similarity Score: {similarity[0][0]:.4f}")
 
